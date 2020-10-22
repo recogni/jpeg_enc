84c3f7dbfaf46281828d65ea79613f11547ba767
Eugene Feinberg <eugene@recogni.com>
2020-10-21 20:36:59 -0700

    jpeg_top_wrap: fix read interface

--- jpeg_top_wrap.sv
+++ jpeg_top_wrap.sv
@@ -39,7 +39,7 @@ module jpeg_top_wrap (
 
     //
     // control state machine
-    // 
+    //
     logic [7:0] pixel_counter, pixel_counter_next;
     logic [1:0] write_state, write_state_next;
     logic rd_full;
@@ -58,7 +58,7 @@ module jpeg_top_wrap (
         pixel_counter_next = pixel_counter;
         jpeg_wr_gnt        = 1'b0;
         jpeg_clk_en        = 1'b0;
-        last_block_next    = last_block;
+        last_block_next    = 1'b0;
         end_interrupt_next = 1'b0;
 
         case ( write_state )
@@ -71,33 +71,35 @@ module jpeg_top_wrap (
                     write_state_next   = COUNTING;
 
                     // Implicit is blocks of 8x8 RGB pixels
-                    pixel_counter_next = 64;
+                    pixel_counter_next = 63;
                 end
             end
 
             // Accepting one of 64 pixel writes of a 8x8
             COUNTING: begin
-                if ( slv.req && slv.we == WRITE ) begin
+                if ( slv.req && slv.wen == WRITE ) begin
                     jpeg_wr_gnt = 1'b1;
                     jpeg_clk_en = 1'b1;
                     if ( pixel_counter == 0 ) begin
-                        write_state_next = PROCESSING;
-                        pixel_counter_next = 33;
+                        write_state_next   = PROCESSING;
+                        pixel_counter_next = 32;
+                    end else begin
+                        pixel_counter_next = pixel_counter - 1;
                     end
-                    pixel_counter_next = pixel_counter - 1;
-                end 
+                end
             end
 
             // While the block is processing, do not accept more writes
             PROCESSING: begin
                 jpeg_clk_en = 1'b1;
-                if ( pixel_counter_next == 1 ) begin
+                if ( pixel_counter == 0 ) begin
                     write_state_next = last_block ? END_PROCESSING : IDLE;
+                end else begin
+                    pixel_counter_next = pixel_counter - 1;
                 end
-                pixel_counter_next = pixel_counter - 1;
             end
 
-            // Continue processing until end of bitstream without accepting 
+            // Continue processing until end of bitstream without accepting
             // new data
             END_PROCESSING: begin
                 jpeg_clk_en = ~rd_full;
@@ -114,14 +116,14 @@ module jpeg_top_wrap (
         endcase
     end
 
-    always_ff @(posedge clk) end_interrupt <= rst ? 1'b0: end_interrupt_next;
-
+    always_ff @(posedge clk) end_interrupt <= rst ? 1'b0 : end_interrupt_next;
+    always_ff @(posedge clk) last_block    <= rst ? 1'b0 : last_block_next;
 
     //
     // JPEG engine clock gating control
     //
     // JPEG engine only runs when there's data being added or for the post
-    // data encoding cycles. To absorb non-contiguous write bursts, the 
+    // data encoding cycles. To absorb non-contiguous write bursts, the
     // clock gate disables the JPEG block on non write cycles until all
     // 64 pixels have been fed into the engine.
     //
@@ -129,12 +131,12 @@ module jpeg_top_wrap (
     // until the end of the bitsream is generated.
     //
     logic jpeg_engine_en;
-    assign jpeg_engine_en = ((write_state == COUNTING)    || 
-                             (write_state == PROCESSING)  || 
-                             (write_state == END_PROCESSING)); 
+    assign jpeg_engine_en = ((write_state == COUNTING)    ||
+                             (write_state == PROCESSING)  ||
+                             (write_state == END_PROCESSING));
 
     // jpeg clock gate
-    pd_idg i_jpeg_icg (
+    pd_icg i_jpeg_icg (
         .clk_en          ( jpeg_clk_en || rst ),
         .ungated_clk_in  ( clk                ),
         .gated_clk       ( jpeg_clk_gated     )
@@ -142,21 +144,23 @@ module jpeg_top_wrap (
 
 
     logic [31:0] fifo_data;
+    logic [31:0] rdata;
     logic [31:0] jpg_data_out;
+    logic [$bits(slv.id)-1:0] r_id;
 
     //
     // JPEG data stream output FIFO
     //
-    // Pushed into on cycles when the JPEG block produces new 32 bit 
+    // Pushed into on cycles when the JPEG block produces new 32 bit
     // words of compressed stream data. Huffman coding and DCT means
     // that the pattern of writes is unpredictable based on the input
-    // data. 
+    // data.
     //
     // The FIFO is 32 entries ( 128 bytes ) deep. The effective compression
     // ratio of JPEG is about 8:1 which means that 3 (RGB) * 8x8 input
     // pixels should on average result in 3*8*8/8 = 24 output bytes.
     //
-    // A fixed threshold interrupt indicates a FIFO level 
+    // A fixed threshold interrupt indicates a FIFO level
 
     fifo_v3 #(
         .DATA_WIDTH(32),   // default data width if the fifo is of type logic
@@ -177,17 +181,17 @@ module jpeg_top_wrap (
         .data_o       ( fifo_data  ),           // output data
         .pop_i        ( ~rd_empty && decode_fifo && slv.req && (slv.wen == READ) && slv.r_valid )            // pop head from queue
     );
-    
-    jpeg_top i_jtag_top (
-        .clk                 ( jpeg_clk_gated     ), 
-        .rst                 ( rst                ), 
-        .end_of_file_signal  ( end_of_file_signal ), 
-        .enable              ( jpeg_engine_en     ), 
-        .data_in             ( slv.wdata          ), 
-        .JPEG_bitstream      ( jpg_data_out       ), 
-        .data_ready          ( data_ready         ), 
 
-        .end_of_file_bitstream_count ( end_of_file_bitstream_count ), 
+    jpeg_top i_jtag_top (
+        .clk                 ( jpeg_clk_gated     ),
+        .rst                 ( rst                ),
+        .end_of_file_signal  ( last_block         ),
+        .enable              ( jpeg_engine_en     ),
+        .data_in             ( slv.wdata          ),
+        .JPEG_bitstream      ( jpg_data_out       ),
+        .data_ready          ( data_ready         ),
+
+        .end_of_file_bitstream_count ( end_of_file_bitstream_count ),
         .eof_data_partial_ready      ( eof_data_partial_ready      )
     );
 
@@ -203,16 +207,34 @@ module jpeg_top_wrap (
     assign decode_end    = (slv.add[9:8] == 2'b11);
 
     // Read data mux
-    assign slv.r_data = decode_fifo  ? fifo_data :
-                        decode_depth ? (32'h0 | rd_depth) :
-                        decode_end   ? (32'h0 | eof_bit_count) :
-                        '0;
+    always_ff @(posedge clk) begin
+        rdata <= decode_fifo  ? fifo_data :
+                 decode_depth ? (32'h0 | rd_depth) :
+                 decode_end   ? (32'h0 | eof_bit_count) :
+                 '0;
+    end
+
+    logic r_valid;
+    always_ff @(posedge clk) begin
+        if ( slv.req && slv.wen == READ ) begin
+            r_id <= slv.id;
+            r_valid <= 1'b1;
+        end else begin
+            r_id    <= '0;
+            r_valid <= 1'b0;
+        end
+    end
+
+    assign slv.r_rdata = rdata;
+    assign slv.r_valid = r_valid;
+    assign slv.r_id    = r_id;
+    assign slv.r_opc   = '0;
 
     assign slv.gnt = (slv.wen == WRITE) ? jpeg_wr_gnt : slv.req;
 
     // Static value for FIFO interrupt
     assign fifo_interrupt = rd_depth > 8;
-    
+
     // Read fifo should never ever fill to the top
     assign error_interrupt = rd_full;
 
